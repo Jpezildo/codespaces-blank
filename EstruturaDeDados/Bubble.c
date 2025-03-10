391308738cfc8458cd6ae5d3ca337a9f7c4a4eda
Jpezildo <163006817+Jpezildo@users.noreply.github.com>
2025-03-10 21:36:09 +0000

    EstruturaDeDados

--- EstruturaDeDados/Bubble.c
+++ EstruturaDeDados/Bubble.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
+
 void bubble_sort(int arr[], int n) {
-int i, j , temp;
-for ( i= n-1; i>0; i--) {
-for (j = 0; j<i; j++) {
-if (arr[j] > arr[j+1]){
-temp = arr[j];
-arr[j] = arr[j+1];
-arr[j+1] = temp; 
-
-
-}
-}
-}
-} 
-
-void bubble_sort_fim_ini(int n, int v[]) {
-for(int i = 0; i < n-1; i++) {
-for(int j = n-1; j>i; j--) {
-if (v[j] < v[j-1]) {
-    int aux = v[j];
-    v[j] = v[j-1];
-    v[j-1] = aux;
-}
-}
-}
-}
-
-void exibir_vetor (int n, int vet[n]) {
-    for(int i = 0; i<n; i++) {
-        printf("[%d]",vet[i]);
-        printf("\n");
+    int i, j, temp;
+    for (i = n - 1; i > 0; i--) {
+        for (j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
 }
 
 
+void bubble_sort_fim_ini(int n, int v[]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = n - 1; j > i; j--) {
+            if (v[j] < v[j - 1]) {
+                int aux = v[j];
+                v[j] = v[j - 1];
+                v[j - 1] = aux;
+            }
+        }
+    }
+}
 
+void exibir_vetor(int n, int vet[]) {
+    for (int i = 0; i < n; i++) {
+        printf("[%d] ", vet[i]);
+    }
+    printf("\n");
+}
+    int main(void) {
+        int v[] = {6, 5, 3, 1, 8, 7, 2, 4};
+int n = sizeof(v) / sizeof(v[0]);;
+        printf("Vetor original:\n");
+        exibir_vetor(n, v);
 
+        bubble_sort_fim_ini(n, v);
 
-
-int main(void) {
-
-
-
-
+        printf("Vetor ordenado:\n");
+        exibir_vetor(n, v);
 
 
 return 0;
+
 }
--- EstruturaDeDados/Selection.c
+++ EstruturaDeDados/Selection.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 
 void selection_sort(int v[], int n) {
-int i, j , min, temp;
-for ( i = 0; i < n-1; i++) {
-    min = i;
-for (j = 0+1; j < n; j++) {
-if (v[j] < v[min]) {
-min = j;
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {  
+            if (v[j] < v[min]) {
+                min = j;
+            }
+        }
+       
+        if (min != i) {  
+            temp = v[min];
+            v[min] = v[i];
+            v[i] = temp;
+        }
+    }
 }
-}   
-if(min !=0) {
-int aux = v[min];
-v[min] = v[0];
-v[0] = aux; 
+
+
+void exibir_vetor(int v[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
 }
-}
-}
-
-
-
-
-
-
-
 
 int main(void) {
+    int v[] = {6, 5, 3, 1, 8, 7, 2, 4}; 
+    int n = sizeof(v) / sizeof(v[0]);   
 
+    printf("Vetor original:\n");
+    exibir_vetor(v, n);
 
+    selection_sort(v, n); 
 
+    printf("Vetor ordenado (Selection Sort):\n");
+    exibir_vetor(v, n);
 
-
-
-return 0;
+    return 0;
 }
